fffa8d3e2b005c0bee3cff8f0a123e87d238bce9
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-05-02 19:05:43 +0430

    feat: create layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import MainLayout from "../components/layout/MainLayout";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
