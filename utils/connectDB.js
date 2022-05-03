8cad8aaebb5b2f509ee0f6739fc6e044c0474c1d
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-05-03 18:42:56 +0430

    feat: create database connection

---
+++ utils/connectDB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  if (mongoose.connections[0].readyState) {
+    console.log("Already connected...");
+    return;
+  }
+  mongoose.connect(
+    "mongodb://localhost:27017/next_shop",
+    {
+      useCreateIndex: true,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("connected to mongodb.");
+    }
+  );
+};
+
+export default connectDB;
